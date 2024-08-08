1fa9af7f4308aa5213a5143db3140c96f45d9a4c
Darren Victoriano <darren.victoriano@gmail.com>
2024-08-07 19:02:55 -0700

    initial commit

---
+++ pahina_server/main.py
@@ -0,0 +1,13 @@
+# my_fastapi_project/main.py
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}

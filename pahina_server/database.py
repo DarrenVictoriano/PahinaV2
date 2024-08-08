d85db7b3131a7ce095bb14a2924af7d44434583e
Darren Victoriano <darren.victoriano@gmail.com>
2024-08-07 19:58:11 -0700

    added database

--- pahina_server/database.py
+++ pahina_server/database.py
@@ -4,3 +4,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 URL_DATABASE = 'sqlite:///./pahina.db'
 
+engine = create_engine(url=URL_DATABASE, connect_args={
+    "check_same_thread": False
+})
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()

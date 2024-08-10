cd9e0e43de0b5ce831c8e5ca70ef69c8e29cc775
Darren Victoriano <darren.victoriano@gmail.com>
2024-08-10 12:02:18 -0700

    wip

--- pahina_server/main.py
+++ pahina_server/main.py
@@ -1,8 +1,33 @@
 # my_fastapi_project/main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Annotated
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+import models
 
 app = FastAPI()
+
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+models.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def read_root():
